1d5c3e243d1efe14c275bd607ccabda12f8a5508
ImOmarKsa78119 <41759540+ImOmarKsa78119@users.noreply.github.com>
2018-07-26 23:19:54 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,27 +1,12 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
- const prefix = "*";
+ const prefix = "برفكسك";
 client.on('ready', () => {
     console.log('I am ready!');
 });
 
 client.on('message', message => {
     if (message.content === 'zg') {
-        message.reply('pong');
-      }
-});
-
-
-
-client.login(process.env.BOT_TOKEN);const Discord = require('discord.js');
-const client = new Discord.Client();
- const prefix = "*";
-client.on('ready', () => {
-    console.log('I am ready!');
-});
-
-client.on('message', message => {
-    if (message.content === 'ping') {
         message.reply('pong');
       }
 });
